8dc789d5a0e1133901fa2a5ac042966a9f811fd0
Pavel Dvorak <954.pavel@email.cz>
2016-02-17 21:00:30 +0100

    predpoklady BI 140

---
+++ 2016/bohumin/140/predpoklady/predpoklady_ts140_2016_02_16.php
@@ -0,0 +1,26 @@
+<?php 
+function predpoklad_1_TS140 ($timestamp) {
+  return 1;
+}
+
+function predpoklad_2_TS140 ($timestamp) {
+  return 3;
+}
+
+function predpoklad_3_TS140 ($timestamp) {
+  return 2;
+}
+
+function predpoklad_4_TS140 ($timestamp) {
+  return 5;
+}
+
+function predpoklad_5_TS140 ($timestamp) {
+  return 6;
+}
+
+function predpoklad_6_TS140 ($timestamp) {
+  return 7;
+}
+
+?>
