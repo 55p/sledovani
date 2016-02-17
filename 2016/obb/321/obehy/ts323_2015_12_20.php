567dd0dd380f10e43018f6d6c9c64b6a6d17b85f
Pavel Dvorak <954.pavel@email.cz>
2016-02-17 21:28:43 +0100

    railjet 553 jako 200553

--- 2016/obb/321/obehy/ts323_2015_12_20.php
+++ 2016/obb/321/obehy/ts323_2015_12_20.php
@@ -3,13 +3,13 @@ $zacatekPlatnosti = '13.12.2015';
 $konecPlatnosti = null; //'x.x.201x 23:59:59';
 
 $obeh[1] = array('70_1', '77_1', '77_2', '1277_1', '758_1');
-$obeh[2] = array('553_1', '76_1', '373_1');
+$obeh[2] = array('200553_1', '76_1', '373_1');
 
 $omezeni['1277_1'] = '6,7';
 $omezeni['77_1'] = '1,2,3,4,5';
 $omezeni['77_2'] = '6,7';
 
-$trasa['553_1'] = array('Wien Hbf;6:58','Graz Hbf;9:33');
+$trasa['200553_1'] = array('Wien Hbf;6:58','Graz Hbf;9:33');
 $trasa['1277_1'] = array('Wien Hbf;16:58','Graz Hbf;19:33');
 $trasa['758_1'] = array('Graz Hbf;20:25','Wien Hbf;23:02');
 
