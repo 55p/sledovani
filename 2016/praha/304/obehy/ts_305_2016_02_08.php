0481dcba091fa7e7794a9ed794162cd8cc10ab56
Pavel Dvorak <954.pavel@email.cz>
2016-02-20 10:25:32 +0100

    TS 305 jen v sobotu

--- 2016/praha/304/obehy/ts_305_2016_02_08.php
+++ 2016/praha/304/obehy/ts_305_2016_02_08.php
@@ -1,5 +1,5 @@
 <?php 
-$zacatekPlatnosti = '8.2.2016';
+$zacatekPlatnosti = '22.2.2016';
 $konecPlatnosti = null; //'x.x.201x 23:59:59';
 
 $obeh[1] = array('553_1', '552_1');
@@ -10,8 +10,8 @@ $typ[553] = 'IC';
 $trasa['552_1'] = array('Přerov;4:55', 'Praha hl.n.;7:15');
 $trasa['553_1'] = array('Praha hl.n.;0:01', 'Přerov;2:47');
 
-$omezeni['552_1'] = 'X6';
-$omezeni['553_1'] = 'X6';
+$omezeni['552_1'] = '6';
+$omezeni['553_1'] = '6';
 
 @$ts305 -> obeh (new Obeh($obeh, $trasa, $omezeni, $typ, $poznamkaObehu, $zacatekPlatnosti, $konecPlatnosti)); 
 
