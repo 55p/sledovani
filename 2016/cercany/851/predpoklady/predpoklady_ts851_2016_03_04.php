4aea508c7eb32d2e6e1bfaf8a52edbf7b65650c9
Pavel Dvorak <954.pavel@email.cz>
2016-03-04 19:28:35 +0100

    PP od 4.3.

---
+++ 2016/cercany/851/predpoklady/predpoklady_ts851_2016_03_04.php
@@ -0,0 +1,26 @@
+<?php 
+function predpoklad_1_TS851 ($timestamp) {
+  return 2;
+}
+
+function predpoklad_2_TS851 ($timestamp) {
+  return 3;
+}
+
+function predpoklad_3_TS851 ($timestamp) {
+  return 4;
+}
+
+function predpoklad_4_TS851 ($timestamp) {
+  return 5;
+}
+
+function predpoklad_5_TS851 ($timestamp) {
+  return 6;
+}
+
+function predpoklad_6_TS851 ($timestamp) {
+  return 1;
+}
+
+?>
