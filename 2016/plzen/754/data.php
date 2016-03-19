82a648dbd7af815544857f352e8291424a7cd968
Pavel Dvorak <954.pavel@email.cz>
2016-03-19 17:20:08 +0100

    TS 754 plzen mimoradny obeh

--- 2016/plzen/754/data.php
+++ 2016/plzen/754/data.php
@@ -3,7 +3,7 @@ $skupiny[] = $ts701 = new TS ('TS 701');
 $skupiny[] = $ts718 = new TS ('TS 718');
 $skupiny[] = $mimor = new TS('mimořádný oběh');
 
-$mimor->obeh(new Obeh(array('-' => array('1','2','3')), array(), array(), array('1' => '', '2' => '', '3' => ''), array()));
+$mimor->obeh(new Obeh(array('1' => array('1'), '2' => array('2'), '3' => array('3'), '4' => array('4'), '5' => array('5')), array(), array(), array('1' => '', '2' => '', '3' => ''), array()));
 
 require __DIR__.$includePath.'/spolecne/obehy_2015_11_22.php';
 @require __DIR__.$includePath.'/spolecne/laky_vse.php';
