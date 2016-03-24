deaea2d8312f0347977e58492a046db4412a9c27
Pavel Dvorak <954.pavel@email.cz>
2016-03-24 20:45:40 +0100

    cercanske 810 od 26.3.

---
+++ 2016/cercany/851/predpoklady/predpoklady_ts852_2016_03_26.php
@@ -0,0 +1,12 @@
+<?php 
+function predpoklad_1_TS852 ($timestamp) {
+  $den = date('N', $timestamp);
+  return 1;
+}
+
+function predpoklad_2_TS852 ($timestamp) {
+  $den = date('N', $timestamp);
+  return 2;
+}
+
+?>
