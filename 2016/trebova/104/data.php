efe0ccd853ab4afc459403cf4797c66fb9df2717
Pavel Dvorak <954.pavel@email.cz>
2016-03-25 10:16:31 +0100

    TS 103 od 1.4. slouceno s 104 a 102

--- 2016/trebova/104/data.php
+++ 2016/trebova/104/data.php
@@ -1,6 +1,7 @@
 <?php
 $skupiny[] = $ts104 = new TS ('TS 104');
 $skupiny[] = $ts102 = new TS ('TS 102');
+$skupiny[] = $ts103 = new TS ('TS 103');
 
 require __DIR__.$includePath.'/spolecne/obehy_2015_11_22.php';
 @require __DIR__.$includePath.'/spolecne/laky_vse.php';
