9ffb2d2588fe24d8e6ebf350f485c97cab5cb67a
Pavel Dvorak <954.pavel@email.cz>
2016-05-14 21:54:06 +0200

    ts 835 plzen blatna, laky

---
+++ 2016/plzen/835/data.php
@@ -0,0 +1,8 @@
+<?php
+$skupiny[] = $ts835 = new TS ('TS 835');
+
+require __DIR__.$includePath.'/spolecne/obehy_2015_11_22.php';
+@require __DIR__.$includePath.'/spolecne/laky_vse.php';
+
+$predpokladySoubor = __DIR__.$includePath.'/spolecne/predpoklady_2015_11_22.php';
+
