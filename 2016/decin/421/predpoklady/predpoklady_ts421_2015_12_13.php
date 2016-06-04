0a60cdcf18ba2c3b72bae084409d4d941a1a9326
Pavel Dvorak <954.pavel@email.cz>
2016-06-04 18:47:17 +0200

    laky + ts 421 decin + 851 cercany vyluka

---
+++ 2016/decin/421/predpoklady/predpoklady_ts421_2015_12_13.php
@@ -0,0 +1,41 @@
+<?php 
+function predpoklad_1_TS421 ($timestamp) {
+  $den = date('N', $timestamp);
+  if ($den == 5) {
+    return 4;
+  }
+  return 2;
+}
+
+function predpoklad_2_TS421 ($timestamp) {
+  return 3;
+}
+
+function predpoklad_3_TS421 ($timestamp) {
+  $den = date('N', $timestamp);
+  if ($den == 5) {
+    return 2;
+  }
+  if ($den == 6) {
+    return 1;
+  }
+  return 4;
+}
+
+function predpoklad_4_TS421 ($timestamp) {
+  return 5;
+}
+
+function predpoklad_5_TS421 ($timestamp) {
+  return 6;
+}
+
+function predpoklad_6_TS421 ($timestamp) {
+  $den = date('N', $timestamp);
+  if ($den == 6) {
+    return 4;
+  }
+  return 1;
+}
+
+?>
