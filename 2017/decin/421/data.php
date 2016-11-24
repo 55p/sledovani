ee9b8952988f7296d299971f66a767efac0bb28a
Pavel Dvorak <954.pavel@email.cz>
2016-11-24 23:23:00 +0100

    TS 421 PJ Decin

---
+++ 2017/decin/421/data.php
@@ -0,0 +1,9 @@
+<?php
+$skupiny[] = $ts421 = new TS ('TS 421');
+
+include __DIR__.$includePath.'/spolecne/obehy_2015_11_22.php';
+@include __DIR__.$includePath.'/spolecne/laky_vse.php';
+
+$predpokladySoubor = __DIR__.$includePath.'/spolecne/predpoklady_2015_11_22.php';
+
+?>
