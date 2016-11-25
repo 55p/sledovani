056258fa1f2f30954c7f8622867ddedc9d195182
Pavel Dvorak <954.pavel@email.cz>
2016-11-25 23:14:26 +0100

    bohumin 741 2017

---
+++ 2017/bohumin/741/data.php
@@ -0,0 +1,13 @@
+<?php
+$skupiny[] = $ts741 = new TS ('TS 741', '754');
+
+if (file_exists(__DIR__.'/typy.php')) {
+  include __DIR__.'/typy.php';
+}
+
+include __DIR__.$includePath.'/spolecne/obehy_2015_11_22.php';
+@include __DIR__.$includePath.'/spolecne/laky_vse.php';
+
+$predpokladySoubor = __DIR__.$includePath.'/spolecne/predpoklady_2015_11_22.php';
+
+?>
---
+++ 2017/bohumin/741/predpoklady/predpoklady_ts741_2016_12_11.php
@@ -0,0 +1,22 @@
+<?php 
+function predpoklad_1_TS741 ($timestamp) {
+  return 2;
+}
+
+function predpoklad_2_TS741 ($timestamp) {
+  return 3;
+}
+
+function predpoklad_3_TS741 ($timestamp) {
+  return 4;
+}
+
+function predpoklad_4_TS741 ($timestamp) {
+  return 5;
+}
+
+function predpoklad_5_TS741 ($timestamp) {
+  return 1;
+}
+
+?>
