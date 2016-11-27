a9646b9146c6f32c232c8c3687bb620443dd559f
Pavel Dvořák <Pavel Dvok>
2016-11-27 12:43:10 +0100

    chyby v tabulkach, jmena vlaku zkusebne

---
+++ 2017/obb/321/jmena_vlaku.php
@@ -0,0 +1,18 @@
+<?php
+$jmenaVlaku['370_1'] = 'Joseph Haydn';
+$jmenaVlaku['371_1'] = 'Joseph Haydn';
+$jmenaVlaku['372_1'] = 'W. A. Mozart';
+$jmenaVlaku['373_1'] = 'W. A. Mozart';
+$jmenaVlaku['579_1'] = 'František Kmoch';
+$jmenaVlaku['580_1'] = 'Leoš Janáček';
+$jmenaVlaku['70_1'] = 'Gustav Mahler';
+$jmenaVlaku['71_1'] = 'Gustav Mahler';
+$jmenaVlaku['72_1'] = 'Bedřich Smetana';
+$jmenaVlaku['73_1'] = 'Bedřich Smetana';
+$jmenaVlaku['74_1'] = 'Franz Schubert';
+$jmenaVlaku['75_1'] = 'Franz Schubert';
+$jmenaVlaku['76_1'] = 'Antonín Dvořák';
+$jmenaVlaku['77_1'] = 'Antonín Dvořák';
+$jmenaVlaku['78_1'] = 'Johann Strauss';
+$jmenaVlaku['79_1'] = 'Johann Strauss';
+
--- 2017/plzen/754/data.php
+++ 2017/plzen/754/data.php
@@ -7,6 +7,9 @@ $skupiny[] = $mimor = new TS('mimořádný oběh');
 if (file_exists(__DIR__.'/typy.php')) {
   include __DIR__.'/typy.php';
 }
+if (file_exists(__DIR__.'/jmena_vlaku.php')) {
+  include __DIR__.'/jmena_vlaku.php';
+}
 
 $mimor->obeh(new Obeh(array('1' => array('1'), '2' => array('2'), '3' => array('3'), '4' => array('4'), '5' => array('5')), array(), array(), array('1' => '', '2' => '', '3' => ''), array()));
 
