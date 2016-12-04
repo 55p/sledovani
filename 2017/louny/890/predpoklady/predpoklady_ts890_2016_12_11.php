5dfa6d414c7b3ea94b7ff7cdc35f3d8556934875
Pavel Dvorak <954.pavel@email.cz>
2016-12-04 10:48:26 +0100

    ts 890 pp louny

---
+++ 2017/louny/890/predpoklady/predpoklady_ts890_2016_12_11.php
@@ -0,0 +1,146 @@
+<?php 
+function predpoklad_1_TS890 ($timestamp) {
+  $den = date('N', $timestamp);
+  if ($den == 6) {
+    return 4;
+  }
+  if ($den == 7) {
+    return 4;
+  }
+  if ($den == 1) {
+    return 1;
+  }
+  if ($den == 2) {
+    return 1;
+  }
+  if ($den == 3) {
+    return 1;
+  }
+  if ($den == 4) {
+    return 1;
+  }
+  if ($den == 5) {
+    return 1;
+  }
+  return 2;
+}
+
+function predpoklad_2_TS890 ($timestamp) {
+  return 3;
+}
+
+function predpoklad_3_TS890 ($timestamp) {
+  $den = date('N', $timestamp);
+  if ($den == 6) {
+    return 2;
+  }
+  if ($den == 7) {
+    return 2;
+  }
+  return 4;
+}
+
+function predpoklad_4_TS890 ($timestamp) {
+  $den = date('N', $timestamp);
+  if ($den == 7) {
+    return 12;
+  }
+  if ($den == 6) {
+    return 6;
+  }
+  if ($den == 1) {
+    return 11;
+  }
+  if ($den == 2) {
+    return 11;
+  }
+  if ($den == 3) {
+    return 11;
+  }
+  if ($den == 4) {
+    return 11;
+  }
+  return 5;
+}
+
+function predpoklad_5_TS890 ($timestamp) {
+  $den = date('N', $timestamp);
+  if ($den == 7) {
+    return 11;
+  }
+  if ($den == 6) {
+    return 5;
+  }
+  return 6;
+}
+
+function predpoklad_6_TS890 ($timestamp) {
+  return 7;
+}
+
+function predpoklad_7_TS890 ($timestamp) {
+  return 8;
+}
+
+function predpoklad_8_TS890 ($timestamp) {
+  return 9;
+}
+
+function predpoklad_9_TS890 ($timestamp) {
+  return 10;
+}
+
+function predpoklad_10_TS890 ($timestamp) {
+  $den = date('N', $timestamp);
+  if ($den == 7) {
+    return 6;
+  }
+  if ($den == 1) {
+    return 5;
+  }
+  if ($den == 2) {
+    return 5;
+  }
+  if ($den == 3) {
+    return 5;
+  }
+  if ($den == 4) {
+    return 5;
+  }
+  if ($den == 6) {
+    return 12;
+  }
+  return 11;
+}
+
+function predpoklad_11_TS890 ($timestamp) {
+  $den = date('N', $timestamp);
+  if ($den == 7) {
+    return 5;
+  }
+  if ($den == 6) {
+    return 11;
+  }
+  return 12;
+}
+
+function predpoklad_12_TS890 ($timestamp) {
+  $den = date('N', $timestamp);
+  if ($den == 1) {
+    return 2;
+  }
+  if ($den == 2) {
+    return 2;
+  }
+  if ($den == 3) {
+    return 2;
+  }
+  if ($den == 4) {
+    return 2;
+  }
+  if ($den == 5) {
+    return 2;
+  }
+  return 1;
+}
+
