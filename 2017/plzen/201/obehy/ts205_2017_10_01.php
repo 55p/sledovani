b6fd3ecd046297a131166e29379893d44d81c213
Pavel Dvořák <Pavel Dvok>
2017-10-04 19:19:41 +0200

    trasy

--- 2017/plzen/201/obehy/ts205_2017_10_01.php
+++ 2017/plzen/201/obehy/ts205_2017_10_01.php
@@ -11,10 +11,12 @@ $omezeni['8945_1'] = '6';
 $omezeni['8948_1'] = 'X5';
 $omezeni['8949_1'] = 'X5';
 
-
-
-
-
+$trasa['8902_1'] = ['Horažďovice předměstí;4:47', 'Plzeň hl.n.;5:54'];
+$trasa['8911_1'] = ['Plzeň hl.n.;16:08', 'Horažďovice předměstí;17:16'];
+$trasa['8944_1'] = ['Nepomuk;9:08', 'Plzeň hl.n.;9:53'];
+$trasa['8945_1'] = ['Plzeň hl.n.;8:08', 'Nepomuk;8:50'];
+$trasa['8948_1'] = ['Nepomuk;14:08', 'Plzeň hl.n.;14:53'];
+$trasa['8949_1'] = ['Plzeň hl.n.;12:08', 'Nepomuk;12:50'];
 
 if (empty($jmenaVlaku)) $jmenaVlaku = [];
 if (empty($zeme)) $zeme = [];
