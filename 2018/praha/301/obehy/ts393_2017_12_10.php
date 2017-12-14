8b7e77ca970c613a4eec88eb4031574d95130acc
Pavel Dvořák <Pavel Dvok>
2017-12-14 11:30:28 +0100

    opravy v TS 303 Plz

--- 2018/praha/301/obehy/ts393_2017_12_10.php
+++ 2018/praha/301/obehy/ts393_2017_12_10.php
@@ -2,8 +2,7 @@
 $zacatekPlatnosti = '10.12.2017';
 $konecPlatnosti = '2.1.2018 23:59:59';
 
-$obeh[1] = ['362_1', '351_1', '356_1', '357_1', '350_1', '363_1'];
-
+$obeh[2] = ['362_1', '351_1', '356_1', '357_1', '350_1', '363_1'];
 
 $trasa['350_1'] = ['Praha hl.n.;17:33','Plzeň hl.n.;18:58'];
 $trasa['351_1'] = ['Plzeň hl.n.;9:01','Praha hl.n.;10:31'];
@@ -11,9 +10,6 @@ $trasa['356_1'] = ['Praha hl.n.;11:33','Plzeň hl.n.;12:58'];
 $trasa['357_1'] = ['Plzeň hl.n.;15:01','Praha hl.n.;16:31'];
 $trasa['362_1'] = ['Praha hl.n.;5:33','Plzeň hl.n.;6:56'];
 $trasa['363_1'] = ['Plzeň hl.n.;21:01','Praha hl.n.;22:31'];
-
-
-
 
 
 if (empty($jmenaVlaku)) $jmenaVlaku = [];
--- 2018/praha/301/predpoklady/predpoklady_ts393_2017_12_10.php
+++
@@ -1,5 +0,0 @@
-<?php 
-function predpoklad_1_TS393 ($timestamp) {
-  return 1;
-}
-
