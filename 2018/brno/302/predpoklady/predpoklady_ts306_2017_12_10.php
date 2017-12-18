982a20a2c293a3b354d761af837189835e0a41ce
Pavel Dvořák <Pavel Dvok>
2017-12-18 17:15:04 +0100

    opravy v TS 303 brno

--- 2018/brno/302/predpoklady/predpoklady_ts306_2017_12_10.php
+++ 2018/brno/302/predpoklady/predpoklady_ts306_2017_12_10.php
@@ -1,17 +1,17 @@
 <?php 
 function predpoklad_1_TS306 ($timestamp) {
-  return 2;
+  return 1;
 }
 
 function predpoklad_2_TS306 ($timestamp) {
-  return 3;
+  return 2;
 }
 
 function predpoklad_3_TS306 ($timestamp) {
-  return 4;
+  return 3;
 }
 
 function predpoklad_4_TS306 ($timestamp) {
-  return 1;
+  return 4;
 }
 
