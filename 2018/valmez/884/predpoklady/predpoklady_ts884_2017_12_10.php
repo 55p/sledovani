753d85f99cd3a509923f16f924fb7171c6364c30
Pavel Dvořák <Pavel Dvok>
2018-02-24 07:40:11 +0100

    změny a sledovačka TS 801 brno

---
+++ 2018/valmez/884/predpoklady/predpoklady_ts884_2017_12_10.php
@@ -0,0 +1,33 @@
+<?php 
+function predpoklad_1_TS884 ($timestamp) {
+  return 2;
+}
+
+function predpoklad_2_TS884 ($timestamp) {
+  return 3;
+}
+
+function predpoklad_3_TS884 ($timestamp) {
+  return 4;
+}
+
+function predpoklad_4_TS884 ($timestamp) {
+  $den = date('N', $timestamp);
+  if ($den == 5) {
+    return 6;
+  }
+  return 5;
+}
+
+function predpoklad_5_TS884 ($timestamp) {
+  $den = date('N', $timestamp);
+  if ($den == 5) {
+    return 5;
+  }
+  return 6;
+}
+
+function predpoklad_6_TS884 ($timestamp) {
+  return 1;
+}
+
