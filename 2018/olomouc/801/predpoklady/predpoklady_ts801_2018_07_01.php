8ffb9d1026266efadda18cb4d2eaa69131dbb0d4
Pavel Dvořák <Pavel Dvok>
2018-03-24 19:45:07 +0100

    laky a opravy v TS 801 a 803 Olomouc

--- 2018/olomouc/801/predpoklady/predpoklady_ts801_2018_07_01.php
+++ 2018/olomouc/801/predpoklady/predpoklady_ts801_2018_07_01.php
@@ -41,32 +41,6 @@ function predpoklad_6_TS801 ($timestamp) {
   if ($den == 6) {
     return 5;
   }
-  return 7;
-}
-
-function predpoklad_7_TS801 ($timestamp) {
-  $den = date('N', $timestamp);
-  if ($den == 5) {
-    return 7;
-  }
-  if ($den == 6) {
-    return 7;
-  }
-  if ($den == 1) {
-    return 1;
-  }
-  if ($den == 2) {
-    return 1;
-  }
-  if ($den == 3) {
-    return 1;
-  }
-  if ($den == 4) {
-    return 1;
-  }
-  if ($den == 7) {
-    return 1;
-  }
   return 1;
 }
 
--- 2018/sumperk/821/predpoklady/predpoklady_ts822_2018_03_19.php
+++
@@ -1,9 +0,0 @@
-<?php 
-function predpoklad_1_TS822 ($timestamp) {
-  return 2;
-}
-
-function predpoklad_2_TS822 ($timestamp) {
-  return 1;
-}
-
