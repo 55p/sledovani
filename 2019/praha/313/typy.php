747daf36e8e5dbefb7b583175bd2bcd4d4b3dda9
Pavel Dvorak <954.pavel@email.cz>
2018-11-23 16:09:33 +0100

    vrsovicke sledovacky 2019

---
+++ 2019/praha/313/typy.php
@@ -0,0 +1,34 @@
+<?php
+$typ[100] = 'EC';
+$typ[101] = 'EC';
+$typ[102] = 'EC';
+$typ[103] = 'EC';
+$typ[104] = 'EC';
+$typ[105] = 'EC';
+$typ[121] = 'Ex';
+$typ[123] = 'Ex';
+$typ[124] = 'Ex';
+$typ[130] = 'EC';
+$typ[131] = 'IC';
+$typ[172] = 'EC';
+$typ[173] = 'EC';
+$typ[270] = 'EC';
+$typ[271] = 'EC';
+$typ[272] = 'EC';
+$typ[273] = 'EC';
+$typ[274] = 'EC';
+$typ[275] = 'EC';
+$typ[276] = 'EC';
+$typ[277] = 'EC';
+$typ[280] = 'EC';
+$typ[281] = 'EC';
+$typ[476] = 'EN';
+$typ[477] = 'EN';
+$typ[572] = 'IC';
+$typ[573] = 'IC';
+$typ[575] = 'IC';
+$typ[590] = 'Ex';
+$typ[591] = 'Ex';
+$typ[816] = 'R';
+$typ[817] = 'R';
+
