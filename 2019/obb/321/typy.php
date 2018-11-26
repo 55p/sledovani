e05707e4538cbf4efae813b6c6889cd19863b9fe
Pavel Dvorak <954.pavel@email.cz>
2018-11-26 21:51:03 +0100

    OCU zapad, 1. cast

---
+++ 2019/obb/321/typy.php
@@ -0,0 +1,22 @@
+<?php
+$typ[370] = 'rj';
+$typ[371] = 'rj';
+$typ[372] = 'rj';
+$typ[374] = 'rj';
+$typ[375] = 'rj';
+$typ[553] = 'rj';
+$typ[570] = 'rj';
+$typ[571] = 'rj';
+$typ[574] = 'rj';
+$typ[70] = 'rj';
+$typ[71] = 'rj';
+$typ[72] = 'rj';
+$typ[73] = 'rj';
+$typ[74] = 'rj';
+$typ[75] = 'rj';
+$typ[76] = 'rj';
+$typ[77] = 'rj';
+$typ[78] = 'rj';
+$typ[79] = 'rj';
+$typ[850] = 'rj';
+
