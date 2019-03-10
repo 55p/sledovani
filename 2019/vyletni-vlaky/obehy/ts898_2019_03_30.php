8922ed377f8f165e62fbf0ad77cd1ff771e48db0
Pavel Dvořák <Pavel Dvok>
2019-03-10 22:18:17 +0100

    vyletni vlaky, prazske diesly

---
+++ 2019/vyletni-vlaky/obehy/ts898_2019_03_30.php
@@ -0,0 +1,95 @@
+<?php 
+$zacatekPlatnosti = '30.3.2019';
+$konecPlatnosti = null;
+
+$obeh['Pražský motoráček'] = ['7759_1', '7750_1', '7751_1', '7752_1', '7753_1', '7754_1', '7755_1', '7756_1', '7757_1', '7758_1'];
+$obeh['Posázavský motoráček'] = ['1563_1', '1562_1', '1565_1', '1564_1'];
+$obeh['Kokořínský rychlík'] = ['1572_1', '1573_1'];
+$obeh['Rakovnický rychlík'] = ['1571_1', '1570_1'];
+$obeh['Podtrosecký rychlík'] = ['1574_1', '1575_1'];
+
+$omezeni['1560_1'] = '6';
+$omezeni['1561_1'] = '6';
+$omezeni['1562_1'] = '6,1904,0105,0805,0507';
+$omezeni['1563_1'] = '6,1904,0105,0805,0507';
+$omezeni['1564_1'] = '7,2204,2810';
+$omezeni['1565_1'] = '7,2204,2810';
+$omezeni['1570_1'] = '25';
+$omezeni['1571_1'] = '25';
+$omezeni['1572_1'] = '25';
+$omezeni['1573_1'] = '25';
+$omezeni['1574_1'] = '25';
+$omezeni['1575_1'] = '25';
+$omezeni['7750_1'] = '25';
+$omezeni['7751_1'] = '25';
+$omezeni['7752_1'] = '25';
+$omezeni['7753_1'] = '25';
+$omezeni['7754_1'] = '25';
+$omezeni['7755_1'] = '25';
+$omezeni['7756_1'] = '25';
+$omezeni['7757_1'] = '25';
+$omezeni['7758_1'] = '25';
+$omezeni['7759_1'] = '25';
+$omezeni['18300_1'] = '6,7';
+$omezeni['18302_1'] = '6,7';
+$omezeni['18303_1'] = '6,7';
+$omezeni['18305_1'] = '6,7';
+$omezeni['18307_1'] = '6,7';
+$omezeni['18308_1'] = '6,7';
+$omezeni['18309_1'] = '6,7';
+$omezeni['18310_1'] = '6,7';
+$omezeni['18311_1'] = '6,7';
+$omezeni['18313_1'] = '6,7';
+
+$trasa['1560_1'] = ['Liberec;15:31','Český Kamenice;17:40'];
+$trasa['1561_1'] = ['Česká Kamenice;8:03','Liberec;10:24'];
+$trasa['1562_1'] = ['Čerčany;12:05','Praha hl.n.;13:35'];
+$trasa['1563_1'] = ['Praha hl.n.;7:39','Čerčany;9:11'];
+$trasa['1564_1'] = ['Čerčany;17:34','Praha hl.n.;19:31'];
+$trasa['1565_1'] = ['Praha hl.n.;12:26','Čerčany;14:07'];
+$trasa['1570_1'] = ['Rakovník;15:26','Praha hl.n.;17:28'];
+$trasa['1571_1'] = ['Praha hl.n.;9:09','Rakovník;10:56'];
+$trasa['1572_1'] = ['Praha-Vršovice;8:27','Mšeno;10:32'];
+$trasa['1573_1'] = ['Mšeno;17:00','Praha-Vršovice;18:56'];
+$trasa['1574_1'] = ['Praha-Vršovice;8:12','Jičín;11:32'];
+$trasa['1575_1'] = ['Jičín;16:54','Praha-Vršovice;19:44'];
+$trasa['7750_1'] = ['Praha hl.n.;8:41','Praha-Zličín;9:09'];
+$trasa['7751_1'] = ['Praha-Zličín;9:48','Praha hl.n.;10:24'];
+$trasa['7752_1'] = ['Praha hl.n.;10:36','Praha-Zličín;11:05'];
+$trasa['7753_1'] = ['Praha-Zličín;11:48','Praha hl.n.;12:24'];
+$trasa['7754_1'] = ['Praha hl.n.;12:29','Praha-Zličín;13:05'];
+$trasa['7755_1'] = ['Praha-Zličín;13:48','Praha hl.n.;14:24'];
+$trasa['7756_1'] = ['Praha hl.n.;14:36','Praha-Zličín;15:05'];
+$trasa['7757_1'] = ['Praha-Zličín;15:48','Praha hl.n.;16:24'];
+$trasa['7758_1'] = ['Praha hl.n.;16:35','Praha-Čakovice;16:55'];
+$trasa['7759_1'] = ['Praha-Čakovice;7:47','Praha hl.n.;8:13'];
+$trasa['18300_1'] = ['Pečky;9:56','Kouřim;10:32'];
+$trasa['18302_1'] = ['Bošice;10:56','Bečváry;11:23'];
+$trasa['18303_1'] = ['Kouřim;10:38','Bošice;10:44'];
+$trasa['18305_1'] = ['Bečváry;11:27','Zásmuky;11:35'];
+$trasa['18307_1'] = ['Zásmuky;12:37','Pečky;13:33'];
+$trasa['18308_1'] = ['Pečky;13:56','Bečváry;14:56'];
+$trasa['18309_1'] = ['Bečváry;15:16','Zásmuky;15:24'];
+$trasa['18310_1'] = ['Bošice;16:45','Kouřim;16:51'];
+$trasa['18311_1'] = ['Zásmuky;16:19','Bošice;16:33'];
+$trasa['18313_1'] = ['Kouřim;17:01','Pečky;17:36'];
+//sem patri stazene trasy
+
+
+
+
+
+
+if (empty($jmenaVlaku)) $jmenaVlaku = [];
+if (empty($zeme)) $zeme = [];
+
+@$ts898 -> obeh (new Obeh2($obeh, ['trasa' => $trasa, 'omezeni' => $omezeni, 'typ' => $typ, 'poznamka' => $poznamkaObehu, 'jmenaVlaku' => $jmenaVlaku, 'zeme' => $zeme], $zacatekPlatnosti, $konecPlatnosti)); 
+
+unset($obeh); 
+unset($trasa); 
+unset($omezeni); 
+unset($zacatekPlatnosti); 
+unset($konecPlatnosti); 
+unset($poznamkaObehu);
+
+
---
+++ 2019/vyletni-vlaky/obehy/ts899_2019_10_01.php
@@ -0,0 +1,127 @@
+<?php 
+$zacatekPlatnosti = '1.10.2019';
+$konecPlatnosti = '28.10.2019 23:59:59';
+
+$obeh['T1 Kamenický motoráček'] = ['2400_1', '2401_1', '2402_1', '2403_1', '2404_1', '2405_1', '2406_1', '2407_1'];
+$obeh['T2 Brtnický cyklovlak'] = ['1799_1', '26024_1', '1913_1'];
+$obeh['T3 Zubrnický motoráček'] = ['15480_1', '15481_1', '15482_1', '15483_1', '15484_1', '15485_1', '15486_1', '15487_1'];
+$obeh['T4 Středohorský motoráček'] = ['18350_1', '18351_1', '18352_1', '18353_1', '18354_1', '18355_1'];
+$obeh['T5 Podřipský motoráček'] = ['18340_1', '18341_1', '18342_1', '18343_1'];
+$obeh['T6 Doupovská dráha'] = ['18360_1', '18363_1', '18362_1', '18365_1', '18366_1', '18367_1', '18368_1', '18371_1', '18370_1', '18373_1', '18374_1', '18375_1'];
+
+//t1
+$omezeni['2400_1'] = '25';
+$omezeni['2401_1'] = '25';
+$omezeni['2402_1'] = '25';
+$omezeni['2403_1'] = '25';
+$omezeni['2404_1'] = '25';
+$omezeni['2405_1'] = '25';
+$omezeni['2406_1'] = '25';
+$omezeni['2407_1'] = '25';
+
+//t2
+$omezeni['1799_1'] = '25';
+$omezeni['26024_1'] = '25';
+$omezeni['1913_1'] = '25';
+
+//t3
+$omezeni['15480_1'] = '25,-1409,-1509';
+$omezeni['15481_1'] = '25,-1409,-1509';
+$omezeni['15482_1'] = '25,-1409,-1509';
+$omezeni['15483_1'] = '25,-1409,-1509';
+$omezeni['15484_1'] = '25,-1409,-1509';
+$omezeni['15485_1'] = '25,-1409,-1509';
+$omezeni['15486_1'] = '25,-1409,-1509';
+$omezeni['15487_1'] = '25,-1409,-1509';
+
+//t4
+$omezeni['18350_1'] = '25';
+$omezeni['18351_1'] = '25';
+$omezeni['18352_1'] = '25';
+$omezeni['18353_1'] = '25';
+$omezeni['18354_1'] = '25';
+$omezeni['18355_1'] = '25';
+
+//t5
+$omezeni['18340_1'] = '25';
+$omezeni['18341_1'] = '25';
+$omezeni['18342_1'] = '25';
+$omezeni['18343_1'] = '25';
+
+//t6
+$omezeni['18360_1'] = '25';
+$omezeni['18362_1'] = '25';
+$omezeni['18363_1'] = '25';
+$omezeni['18365_1'] = '25';
+$omezeni['18366_1'] = '25';
+$omezeni['18367_1'] = '25';
+$omezeni['18368_1'] = '25';
+$omezeni['18370_1'] = '25';
+$omezeni['18371_1'] = '25';
+$omezeni['18373_1'] = '25';
+$omezeni['18374_1'] = '25';
+$omezeni['18375_1'] = '25';
+
+$trasa['1799_1'] = ['Děčín hl.n.;9:37','Mikulášovice dolní nádraží;11:22'];
+$trasa['1913_1'] = ['Rumburk;16:53','Děčín hl.n.;18:18'];
+$trasa['2400_1'] = ['Česká Kamenice;9:15','Kamenický Šenov;9:30'];
+$trasa['2401_1'] = ['Kamenický Šenov;10:25','Česká Kamenice;10:40'];
+$trasa['2402_1'] = ['Česká Kamenice;11:15','Kamenický Šenov;11:30'];
+$trasa['2403_1'] = ['Kamenický Šenov;12:25','Česká Kamenice;12:40'];
+$trasa['2404_1'] = ['Česká Kamenice;13:15','Kamenický Šenov;13:30'];
+$trasa['2405_1'] = ['Kamenický Šenov;14:25','Česká Kamenice;14:40'];
+$trasa['2406_1'] = ['Česká Kamenice;15:15','Kamenický Šenov;15:30'];
+$trasa['2407_1'] = ['Kamenický Šenov;16:25','Česká Kamenice;16:40'];
+$trasa['2408_1'] = ['Česká Kamenice;17:15','Kamenický Šenov;17:30'];
+$trasa['2409_1'] = ['Kamenický Šenov;18:25','Česká Kamenice;18:40'];
+$trasa['15480_1'] = ['Ústí nad Labem-Střekov;10:10','Zubrnice;10:43'];
+$trasa['15481_1'] = ['Zubrnice;11:00','Ústí nad Labem-Střekov;11:38'];
+$trasa['15482_1'] = ['Ústí nad Labem-Střekov;12:10','Zubrnice;12:43'];
+$trasa['15483_1'] = ['Zubrnice;13:00','Ústí nad Labem-Střekov;13:38'];
+$trasa['15484_1'] = ['Ústí nad Labem-Střekov;14:10','Zubrnice;14:43'];
+$trasa['15485_1'] = ['Zubrnice;15:00','Ústí nad Labem-Střekov;15:38'];
+$trasa['15486_1'] = ['Ústí nad Labem-Střekov;16:10','Zubrnice;16:43'];
+$trasa['15487_1'] = ['Zubrnice;17:00','Ústí nad Labem-Střekov;17:38'];
+$trasa['18340_1'] = ['Roudnice nad Labem;9:56','Libochovice;11:06'];
+$trasa['18341_1'] = ['Libochovice;11:48','Roudnice nad Labem;13:09'];
+$trasa['18342_1'] = ['Roudnice nad Labem;13:51','Libochovice;15:01'];
+$trasa['18343_1'] = ['Libochovice;16:48','Roudnice nad Labem;18:00'];
+$trasa['18350_1'] = ['Lovosice;8:35','Most;9:48'];
+$trasa['18351_1'] = ['Most;10:18','Lovosice;11:28'];
+$trasa['18352_1'] = ['Lovosice;12:35','Most;13:48'];
+$trasa['18353_1'] = ['Most;14:18','Lovosice;15:28'];
+$trasa['18354_1'] = ['Lovosice;16:35','Most;17:48'];
+$trasa['18355_1'] = ['Most;18:18','Lovosice;19:28'];
+$trasa['18360_1'] = ['Kadaň;8:21','Kadaňský Rohozec;9:07'];
+$trasa['18362_1'] = ['Vilémov u Kadaně;9:33','Krásný Dvůr;9:48'];
+$trasa['18363_1'] = ['Kadňský Rohozec;9:12','Vilémov u Kadaně;9:30'];
+$trasa['18365_1'] = ['Krásný Dvůr;9:52','Kadaň-Prunéřov;10:42'];
+$trasa['18366_1'] = ['Kadaň-Prunéřov;10:51','Podbořany;12:00'];
+$trasa['18367_1'] = ['Podbořany;12:20','Kadaň;13:23'];
+$trasa['18368_1'] = ['Kadaň;14:20','Radonice u Kadaně;14:58'];
+$trasa['18370_1'] = ['Vilémov u Kadaně;15:24','Krásný Dvůr;15:38'];
+$trasa['18371_1'] = ['Radonice u Kadaně;15:10','Vilémov u Kadaně;15:21'];
+$trasa['18373_1'] = ['Krásný Dvůr;15:46','Kadaň-Prunéřov;16:36'];
+$trasa['18374_1'] = ['Kadaň-Prunéřov;16:51','Podbořany;18:00'];
+$trasa['18375_1'] = ['Podbořany;18:20','Kadaň;19:23'];
+$trasa['26024_1'] = ['Mikulášovice dolní nádraží;11:56','Krásná Lípa;12:32;12:44','Rumburk;12:50'];
+//sem patri stazene trasy
+
+
+
+
+
+
+if (empty($jmenaVlaku)) $jmenaVlaku = [];
+if (empty($zeme)) $zeme = [];
+
+@$ts899 -> obeh (new Obeh2($obeh, ['trasa' => $trasa, 'omezeni' => $omezeni, 'typ' => $typ, 'poznamka' => $poznamkaObehu, 'jmenaVlaku' => $jmenaVlaku, 'zeme' => $zeme], $zacatekPlatnosti, $konecPlatnosti)); 
+
+unset($obeh); 
+unset($trasa); 
+unset($omezeni); 
+unset($zacatekPlatnosti); 
+unset($konecPlatnosti); 
+unset($poznamkaObehu);
+
+
