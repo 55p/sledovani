333f64b11fb6932d133c8a63c78df3036b8c695a
Pavel Dvořák <Pavel Dvok>
2019-12-12 16:35:31 +0100

    laky a obehy 2020

--- 2020/bohumin/421/data.php
+++ 2020/bohumin/421/data.php
@@ -6,3 +6,13 @@ $dopravce[-1] = 1154;
 $dir = __DIR__;
 include __DIR__.$includePath.'/spolecne/superdata_2018_09_03.php';
 
+unset($lak['elefant_2_cerveny']);
+
+$lak['slon_ova_24'] = array(471024);
+$lak['slon_ova_25'] = array(471025);
+$lak['slon_ova_35'] = array(471035);
+$lak['slon_ova_54'] = array(471054);
+$lak['slon_ova_55'] = array(471055);
+$lak['slon_ova_56'] = array(471056);
+$lak['slon_ova_57'] = array(471057);
+$lak['slon_ova_58'] = array(471058);
